ee4ed5c1205298a7bcd2fe288967edd7b97ddf75
giotramu <14157578+giotramu@users.noreply.github.com>
2020-05-07 18:10:49 +0200

    chore: update some minor stuffs

--- packages/design-tokens/.stylelintrc.js
+++ packages/design-tokens/.stylelintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: '@giotramu/stylelint-config/scss'
+  extends: '@giotramu/stylelint-config/scss',
+  rules: {
+    'scss/operator-no-unspaced': null,
+    'property-no-unknown': null
+  }
 };
