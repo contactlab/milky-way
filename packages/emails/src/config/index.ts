23e9e73d8d8bf63b4a775a845cafed1b9860d42f
giotramu <14157578+giotramu@users.noreply.github.com>
2020-05-12 18:13:27 +0200

    feat(setup): github page

--- packages/emails/src/config/index.ts
+++
@@ -1,8 +0,0 @@
-// --- resources
-export const logoSrc =
-  'https://cdn.jsdelivr.net/npm/@contactlab/marketing-cloud-logo/resources/png/logo-mc-full-positive-593x60.png';
-
-// --- links
-export const logoHref = 'http://mc.contactlab.it';
-export const loginHref = 'https://login.contactlab.it';
-export const profilePageHref = 'https://mc.contactlab.it/#/ac/profile';
