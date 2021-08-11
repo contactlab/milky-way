1088843fef5d3d15614f68fac3474cf742153383
giotramu <14157578+giotramu@users.noreply.github.com>
2021-08-11 17:45:50 +0200

    Fix media-queries

--- packages/design-tokens/constants/custom-media.ts
+++ packages/design-tokens/constants/custom-media.ts
@@ -3,27 +3,27 @@
  */
 
 // --- Mobile first
-export const fromXSmall = '(min-width: 36em)'; // 576px
-export const fromSmall = '(min-width: 48em)'; // 768px
-export const fromMedium = '(min-width: 62em)'; // 992px
-export const fromLarge = '(min-width: 75em)'; // 1200px
-export const fromXLarge = '(min-width: 100em)'; // 1600px
+export const fromXSmall = 'min-width: 36em'; // 576px
+export const fromSmall = 'min-width: 48em'; // 768px
+export const fromMedium = 'min-width: 62em'; // 992px
+export const fromLarge = 'min-width: 75em'; // 1200px
+export const fromXLarge = 'min-width: 100em'; // 1600px
 
 // --- Desktop first
-export const toXSmall = '(max-width: 36em)'; // 576px
-export const toSmall = '(max-width: 48em)'; // 768px
-export const toMedium = '(max-width: 62em)'; // 992px
-export const toLarge = '(max-width: 75em)'; // 1200px
-export const toXLarge = '(max-width: 100em)'; // 1600px
+export const toXSmall = 'max-width: 36em'; // 576px
+export const toSmall = 'max-width: 48em'; // 768px
+export const toMedium = 'max-width: 62em'; // 992px
+export const toLarge = 'max-width: 75em'; // 1200px
+export const toXLarge = 'max-width: 100em'; // 1600px
 
 // --- Orientation
-export const landscape = '(orientation: landscape)';
-export const portrait = '(orientation: portrait)';
+export const landscape = 'orientation: landscape';
+export const portrait = 'orientation: portrait';
 
 // --- a11y
-export const reducedMotion = '(prefers-reduced-motion: reduce)';
-export const lightScheme = '(prefers-color-scheme: light)';
-export const darkScheme = '(prefers-color-scheme: dark)';
+export const reducedMotion = 'prefers-reduced-motion: reduce';
+export const lightScheme = 'prefers-color-scheme: light';
+export const darkScheme = 'prefers-color-scheme: dark';
 
 // --- Misc
 export const finePointer = '(hover: hover) and (pointer: fine)';
