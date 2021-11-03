d36ade35cfbb98ce50e00a881d55e9073b53232a
giotramu <14157578+giotramu@users.noreply.github.com>
2021-11-03 12:46:22 +0100

    Encoding Artworks in Base64

---
+++ packages/design-tokens/artworks/index.ts
@@ -0,0 +1,17 @@
+export type ArtworkType =
+  | 'error-mobile'
+  | 'network'
+  | 'no-chart-boxed'
+  | 'no-chart'
+  | 'no-data-boxed'
+  | 'no-data'
+  | 'no-image-boxed'
+  | 'no-image'
+  | 'settings-animated'
+  | 'settings'
+  | 'warn-api'
+  | 'warn-auth'
+  | 'warn-generic'
+  | 'warn-server'
+  | 'warn-sync'
+  | 'warn-ui';
